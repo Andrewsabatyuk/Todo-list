6c96fb75ef6e178a9a72dbb4f32f42900e8600bc
Андрій <64520200+Andrewsabatyuk@users.noreply.github.com>
2020-11-19 17:01:31 +0200

    v 1.06
    add my own hook

--- todo-list/src/Todo/AddTodo.js
+++ todo-list/src/Todo/AddTodo.js
@@ -1,29 +1,42 @@
-import React, {useState} from 'react';
-import propTypes from 'prop-types';
+import React, { useState } from "react";
+import propTypes from "prop-types";
 
-function AddTodo({onCreate}){
-    const[value, setValue]= useState('')
+function useInputvalue(defaultValue = "") {
+  const [value, setValue] = useState(defaultValue);
 
-    function submitHandler (event){
-        event.preventDefault()
+  return {
+    bind: {
+      value,
+      onChange: (event) => setValue(event.target.value),
+    },
+    clear: () => setValue(""),
+    value: () => value,
+  };
+}
 
-        if (value.trim()){
-           onCreate(value)
-           setValue('')
-        }
+function AddTodo({ onCreate }) {
+  const input = useInputvalue("");
+
+  function submitHandler(event) {
+    event.preventDefault();
+
+    if (input.value.trim()) {
+      onCreate(input.value());
+      input.clear();
+      //    setValue('')
     }
+  }
 
-return(
-    <form style={{marginBottom: '1rem'}} onSubmit ={submitHandler} >
-        <input value={value} onChange={event => setValue(event.target.value)} />
-        <button type='submit'>Add todo</button>
+  return (
+    <form style={{ marginBottom: "1rem" }} onSubmit={submitHandler}>
+      <input {...input.bind} />
+      <button type="submit">Add todo</button>
     </form>
-)
+  );
 }
 
+AddTodo.prototype = {
+  onCreate: propTypes.func.isRequired,
+};
 
-AddTodo.prototype ={
-    onCreate: propTypes.func.isRequired
-}
-
-export default AddTodo
+export default AddTodo;
