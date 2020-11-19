7f3e6932727b353f7f79325069d09399fd5c1d70
Андрій <64520200+Andrewsabatyuk@users.noreply.github.com>
2020-11-19 16:43:04 +0200

    v 1.05
    almost final project, add new component AddTodo with input for adding new events and submit button

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,6 +1,7 @@
-import React from "react"
-import Todolist from "./Todo/Todolist"
-import Context from './context'
+import React from "react";
+import Todolist from "./Todo/Todolist";
+import Context from "./context";
+import AddTodo from "./Todo/AddTodo";
 
 function App() {
   const [todos, setTodos] = React.useState([
@@ -9,31 +10,38 @@ function App() {
     { id: 3, completed: false, title: "Купити молоко" },
     { id: 4, completed: false, title: "Вивчити React" },
     { id: 5, completed: false, title: "get out of this" },
-  ])
-  
+  ]);
 
-
-  function toggleTodo(id){
-    setTodos(  todos.map( todo =>{
-      if (todo.id === id){
-        todo.completed = !todo.completed
-      }
-      return todo
-    })
-    )
+  function toggleTodo(id) {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      })
+    );
   }
 
-  function removeTodo(id){
-  setTodos(todos.filter(todo => todo.id !== id))    
+  function removeTodo(id) {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  }
+
+  function addTodo(title) {
+    setTodos(todos.concat([{ title, id: Date.now(), completed: false }]));
   }
 
   return (
-    <Context.Provider value={{removeTodo}}>
-    <div className="wrapper">
-      <h1 className="header-todo">Todo list</h1>
-
-      <Todolist todos={todos}  onToggle={toggleTodo}/>
-    </div>
+    <Context.Provider value={{ removeTodo }}>
+      <div className="wrapper">
+        <h1 className="header-todo">Todo list</h1>
+        <AddTodo onCreate={addTodo} />
+        {todos.length ? (
+          <Todolist todos={todos} onToggle={toggleTodo} />
+        ) : (
+          <p>No todos!</p>
+        )}
+      </div>
     </Context.Provider>
   );
 }
---
+++ todo-list/src/Todo/AddTodo.js
@@ -0,0 +1,29 @@
+import React, {useState} from 'react';
+import propTypes from 'prop-types';
+
+function AddTodo({onCreate}){
+    const[value, setValue]= useState('')
+
+    function submitHandler (event){
+        event.preventDefault()
+
+        if (value.trim()){
+           onCreate(value)
+           setValue('')
+        }
+    }
+
+return(
+    <form style={{marginBottom: '1rem'}} onSubmit ={submitHandler} >
+        <input value={value} onChange={event => setValue(event.target.value)} />
+        <button type='submit'>Add todo</button>
+    </form>
+)
+}
+
+
+AddTodo.prototype ={
+    onCreate: propTypes.func.isRequired
+}
+
+export default AddTodo
